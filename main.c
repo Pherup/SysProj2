f13dab5a076833dfed78de5ea297c084b2350d03
Pherup <philyjoe@gmail.com>
2018-11-12 21:58:35 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "simpleCSVsorter.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+    dirsearch(argc,argv);
+
+    return 0;
+}
